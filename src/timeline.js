3038b6d9261f02f2a04db1af4f124ea6ea0d4241
iqbalkang225 <iqbal.kang.cohort225@gmail.com>
2024-02-05 15:05:49 -0800

    test commit after changing the email

--- src/timeline.js
+++ src/timeline.js
@@ -1,6 +1,3 @@
-// import { gsap } from '/node_modules/gsap/index.js';
-// import { ScrollTrigger } from '/node_modules/gsap/ScrollTrigger.js';
-
 gsap.registerPlugin(ScrollTrigger);
 
 const timelineSection = document.querySelector('.timeline');
